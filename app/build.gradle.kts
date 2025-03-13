5dca243f43b91760151742972f6c04bab4ee5dae
Duycongxuan <congxuanduy2004@gmail.com>
2025-03-13 11:52:35 +0700

    Build homePage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
 }
--- app/src/main/java/com/example/cooksmart_n19/activities/MainActivity.java
+++ app/src/main/java/com/example/cooksmart_n19/activities/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.cooksmart_n19.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.cooksmart_n19.R;
 
@@ -17,10 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
 }
